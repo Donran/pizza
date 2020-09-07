c2f6897ba85afd4ebdcea5a6abd0b4ce9f0b6c8f
Erik <35975961+KermitTheFrogerino@users.noreply.github.com>
2020-09-07 15:13:39 +0200

    testinit

---
+++ Selenium/setup.sh
@@ -0,0 +1,2 @@
+pip3 install webdriverdownloader
+webdriverdownloader chrome:2.38
--- Selenium/tests.py
+++ Selenium/tests.py
@@ -1,4 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.chrome()
+driver = webdriver.Chrome()
+
+driver.get("http://localhost:5500/public/")
+
+try:
+    pizza1 = driver.find_element_by_id("pizza_Margarita")
+    print(pizza1)
+except print(0):
+    pass
