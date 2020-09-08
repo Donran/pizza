986bd920d40ec09f56d4419659bf628e119a4928
Erik <35975961+KermitTheFrogerino@users.noreply.github.com>
2020-09-08 11:13:01 +0200

    Closed hours test

--- tests/run.py
+++ tests/run.py
@@ -1,20 +1,24 @@
-import os
+# Imports Selenium python module
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# Imports all of the classes from the files in the "pythonTests" directory
 from pythonTests.menuTest import MenuTest
 from pythonTests.infoTest import InfoTest
 from pythonTests.openingHoursTest import OpeningHoursTest
 from pythonTests.titleTest import TitleTest
 from pythonTests.footerTest import FooterTest
+from pythonTests.daysClodesTest import DaysClodesTest
 
+# Configures google web options 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
 
+# Ask what host you want
 print("""1. localhost
 2. public website""")
 choice = input("Choose: ")
 
+#Configures two alternatives
 url = ""
 if choice == "1":
     port = input("Choose port (default should be 5500): ")
@@ -24,6 +28,7 @@ elif choice == "2":
 
 print("Fetching from: " + url)
 
+# Opens the chosen URL
 driver.get(url)
 
 baseDivPath = "/html/body/div[@class='ContentDiv']"
@@ -33,3 +38,4 @@ OpeningHoursTest(driver, baseDivPath)
 TitleTest(driver, baseDivPath)
 MenuTest(driver, baseDivPath)
 FooterTest(driver, baseDivPath)
+DaysClodesTest(driver, baseDivPath)
