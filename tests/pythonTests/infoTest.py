279af62b37fae2408f2f6d199c3ee3b5b7f8e9b3
Erik <35975961+KermitTheFrogerino@users.noreply.github.com>
2020-09-10 10:21:03 +0200

    Added test for input and button for the overlay

--- tests/pythonTests/infoTest.py
+++ tests/pythonTests/infoTest.py
@@ -7,22 +7,24 @@ class InfoTest:
         divpath = baseDivPath + "/div[@id='InfoDiv']"
         print("")
         # Find address
-        address = driver.find_element_by_xpath(
-            divpath + "/a[@href='https://goo.gl/maps/2aqFdNDscvCgKKQR9']").text
+        address = driver.find_elements_by_xpath(
+            divpath + "/div[@id='addressDiv']/a[@href='https://goo.gl/maps/2aqFdNDscvCgKKQR9']")
         # If not true, returns error
-        assert address != ""
         print(address)
+        assert len(address) != 0
+        for item in range(len(address)):
+             print(address[item].text)
 
         # Find email
         address = driver.find_element_by_xpath(
-            divpath + "/a[@href='mailto:info@fantastic4group.gitlab.io']").text
+            divpath + "/div[@id='otherInfoDiv']/a[@href='mailto:info@fantastic4group.gitlab.io']").text
         # If not true, returns error
         assert address != ""
         print(address)
 
         # Find phone number
         address = driver.find_element_by_xpath(
-            divpath + "/a[@href='tel:0630-555-555']").text
+            divpath + "/div[@id='otherInfoDiv']/a[@href='tel:0630-555-555']").text
         # If not true, returns error
         assert address != ""
         print(address)
--- tests/pythonTests/orderTest.py
+++ tests/pythonTests/orderTest.py
@@ -4,8 +4,15 @@ from selenium import webdriver
 class OrderTest:
     def __init__(self, driver: webdriver.Chrome, baseDivPath: str):
         print("")
+        # Find order button
         orderButton = driver.find_element_by_id("orderButton")
         assert orderButton.text == "BESTÄLL"
         print(orderButton.text)
+        # Find order overlay
         overlay= driver.find_element_by_id("orderOverlay")
         assert overlay
+        orderButton.click()
+        # assert overlay.get_attribute("data-state") == "visible"
+        
+        inputElement = driver.find_element_by_id("orderInput")
+        confirmButton = driver.find_element_by_id("orderConfirmButton")
