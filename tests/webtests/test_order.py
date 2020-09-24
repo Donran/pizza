b916f7688b37f0708e83e922c5abcfaad4833379
Pluppen <eliass.juremalm@gmail.com>
2020-09-24 13:34:45 +0200

    Fix tests to work without ids

--- tests/webtests/test_order.py
+++ tests/webtests/test_order.py
@@ -2,42 +2,86 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from web_test_base import WebTestBase
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 
-def set_value_and_check(self, value, driver, confirmButton, inputElement):
-        # Change the value of the input element to a valid zip-code
-        driver.execute_script(
-            "arguments[0].setAttribute('value', arguments[1])", inputElement, value)
-        confirmButton.click()
+def set_value_and_check(self, zipcode, driver, confirm_button, input_element):
+
+        input_element.send_keys(zipcode)
+        confirm_button.click()
 
         # Checks if input value outputs the correct message
-        if driver.find_element(By.ID, "orderStatus").text != "Vi levererar inte till dig":
+        if driver.find_element(By.CLASS_NAME, "order-info").text != "Tyvärr kör vi inte ut inom detta område":
             return True
         else:
             return False
 
 class TestOrder(WebTestBase):
 
-    def test_order(self):
+    def test_order_overlay_shows(self):
         driver = self.driver
         driver.get(self.WEBSITE_URL)
 
         # Find order button by id
-        orderButton = driver.find_element(By.ID, "orderButton")
-        self.assertEqual(orderButton.text, "UTKÖRNING")
+        order_button = driver.find_element(By.CLASS_NAME, "order-button")
+        self.assertEqual(order_button.text, "UTKÖRNING")
 
         try:
             # Find order overlay by id
-            overlay = driver.find_element(By.ID, "orderOverlay")
+            overlay = driver.find_element(By.CLASS_NAME, "order-overlay")
         except NoSuchElementException:
             self.fail("Could not find order overlay.")
 
-        orderButton.click()
-        self.assertEqual(overlay.get_attribute("data-state"), "visible")
+    def test_valid_zipcode_works(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
 
-        # Find input by id
-        inputElement = driver.find_element(By.ID, "orderInput")
-        # Find confirm button by id
-        confirmButton = driver.find_element(By.ID, "orderConfirmButton")
-        # Sets value of input to a zip code
-        self.assertEqual(set_value_and_check(self, "98139", driver, confirmButton, inputElement), True)
-        self.assertEqual(set_value_and_check(self, "89488", driver, confirmButton, inputElement), False)
+        order_button = driver.find_element(By.CLASS_NAME, "order-button")
+        order_button.click()
+
+        overlay = driver.find_element(By.CLASS_NAME, "order-overlay")
+
+        input_element = overlay.find_element(By.TAG_NAME, "input")
+        confirm_button = driver.find_element(By.CLASS_NAME, "order-confirm-button")
+
+        input_element.send_keys("98139")
+        confirm_button.click()
+
+        self.assertIn("Vi levererar till dig", driver.page_source)
+
+    def test_invalid_zipcode_works(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+
+        order_button = driver.find_element(By.CLASS_NAME, "order-button")
+        order_button.click()
+        
+        overlay = driver.find_element(By.CLASS_NAME, "order-overlay")
+
+        input_element = overlay.find_element(By.TAG_NAME, "input")
+        confirm_button = driver.find_element(By.CLASS_NAME, "order-confirm-button")
+
+        input_element.send_keys("123456")
+        confirm_button.click()
+
+        self.assertIn("Tyvärr kör vi inte ut inom detta område", driver.page_source)
+
+    def test_keyshortcuts_works(self):
+        driver = self.driver
+        driver.get(self.WEBSITE_URL)
+
+        order_button = driver.find_element(By.CLASS_NAME, "order-button")
+        order_button.click()
+
+        overlay = driver.find_element(By.CLASS_NAME, "order-overlay")
+
+        input_element = overlay.find_element(By.TAG_NAME, "input")
+        input_element.send_keys("98139")
+        input_element.send_keys(Keys.RETURN)
+
+        self.assertIn("Vi levererar till dig", driver.page_source)
+
+        driver.find_element(By.TAG_NAME, "body").send_keys(Keys.ESCAPE)
+
+        self.assertEqual(overlay.get_attribute("data-state"), "hidden")
+
+
